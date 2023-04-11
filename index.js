0b3ea677095b4cf0fa255cd29e77e028655bdcf4
Haziel Benito <126825067+HazielDev@users.noreply.github.com>
2023-04-10 18:39:01 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ let elmo = document.getElementById("img-elmo");
 
 if (window.innerWidth <= 800) {
 
-    let pos = (document.getElementById("elmo").offsetWidth / 6);
-    let cont = (document.getElementById("elmo")).offsetWidth;
+    let pos = (document.getElementById("mini-menu").offsetWidth / 12);
+    let pos1 = document.getElementById("boton-acerca").offsetLeft;
 
     switch(px){
 
@@ -14,9 +14,13 @@ if (window.innerWidth <= 800) {
         // case 420: px = 140; break;
         // case 600: px = 270; break;
 
+        //  case 235: px = 0; break;
+        //  case 420: px = pos; break;
+        //  case 600: px = pos*2; break;
+
         case 235: px = pos; break;
-        case 420: px = pos*2.5; break;
-        case 600: px = pos*4; break;
+        case 420: px = pos*4.5; break;
+        case 600: px = pos*8; break;
 
     }
 
