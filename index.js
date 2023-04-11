df85492ade044a52522bf02458bdb727d118045d
Haziel Benito <126825067+HazielDev@users.noreply.github.com>
2023-04-10 18:13:15 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 
 function mover(px){
 
-let acerca = document.getElementById("boton-guep");
 let elmo = document.getElementById("img-elmo");
 
 if (window.innerWidth <= 800) {
 
+    let pos = (document.getElementById("elmo").offsetWidth / 6);
+    let cont = (document.getElementById("elmo")).offsetWidth;
+
     switch(px){
 
-        case 235: px = 15; break;
-        case 420: px = 140; break;
-        case 600: px = 270; break;
+        // case 235: px = 15; break;
+        // case 420: px = 140; break;
+        // case 600: px = 270; break;
+
+        case 235: px = pos; break;
+        case 420: px = pos*2.5; break;
+        case 600: px = pos*4; break;
 
     }
 
